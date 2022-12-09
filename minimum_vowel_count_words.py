eb7ff3d613164238b2614b2195e7510b20207123
Gera Suvarnaraju <98529286+Suvarna123456789@users.noreply.github.com>
2022-12-09 01:30:56 -0800

    def fun(n):     v="aeiouAEIOU"     c=0     for i in range(0,len(n)):         if n[i] in v:             c+=1     return c a=input() l=[] s=a.split() for i in s:     c=fun(i)     l.append(c) d=0 if len(l)==1:     print(1) else:     print(l.count(min(l)))

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,18 @@
+def fun(n):
+    v="aeiouAEIOU"
+    c=0
+    for i in range(0,len(n)):
+        if n[i] in v:
+            c+=1
+    return c
+a=input()
+l=[]
+s=a.split()
+for i in s:
+    c=fun(i)
+    l.append(c)
+d=0
+if len(l)==1:
+    print(1)
+else:
+    print(l.count(min(l)))
