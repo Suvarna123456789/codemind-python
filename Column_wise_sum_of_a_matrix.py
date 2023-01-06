ffccf08f88833c58eb4961d8f926c27a36bde46e
Gera Suvarnaraju <98529286+Suvarna123456789@users.noreply.github.com>
2023-01-05 21:30:28 -0800

    r,c=map(int,input().split()) row=[0 for i in range(c)] d=[row.copy() for i in range(r)] a=[0 for i in range(c)] for i in range(r):     val=list(map(int,input().split()))     for j in range(c):         d[i][j]=val[j] for i in range(c):     for j in range(r):         a[i]+=d[j][i] print(*a)

--- Column_wise_sum_of_a_matrix.py
+++ Column_wise_sum_of_a_matrix.py
@@ -1,15 +1,12 @@
-def col(d,r,c):
-    r_s=[0 for i in range(c)]
-    for i in range(c):
-        for j in range(r):
-            r_s[i]+=d[j][i]
-    return r_s
 r,c=map(int,input().split())
 row=[0 for i in range(c)]
 d=[row.copy() for i in range(r)]
+a=[0 for i in range(c)]
 for i in range(r):
     val=list(map(int,input().split()))
     for j in range(c):
         d[i][j]=val[j]
-res=col(d,r,c)
-print(*res)
+for i in range(c):
+    for j in range(r):
+        a[i]+=d[j][i]
+print(*a)
